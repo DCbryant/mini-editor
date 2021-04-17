0711cf901f89eb5ec5b1733a83edfbdcbf424b14
dingcong <dingcong@chanjet.com>
2021-04-17 14:09:07 +0800

    feat(config): add css ts webpack config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-
+import './index.css'
 const add = (a: number, b: number) => {
   return a + b
 }
+
+
+console.log(add(1, 2))
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.css/,
+        use:['style-loader', 'css-loader']
+      },
     ]
   },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    host: '127.0.0.1'
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ]
 };
