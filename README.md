781fa752b85e612fade0d04f6dac905c9131e149
dingcong <dingcong@chanjet.com>
2021-04-22 15:28:59 +0800

    更新.gitignore readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mini-editor
+
+简单的富文本编辑器，支持设置文字大小，颜色，加粗
+
+## 打包
+```bash
+npm run build
+```
+
+## 运行
+```bash
+npm run dev
+```
--- dist/bundle.js
+++ dist/bundle.js
@@ -10,13 +10,13 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./node_modules/css-loader/dist/cjs.js!./src/index.css":
-/*!*************************************************************!*\
-  !*** ./node_modules/css-loader/dist/cjs.js!./src/index.css ***!
-  \*************************************************************/
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/editor/index.css":
+/*!********************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/editor/index.css ***!
+  \********************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);\n// Imports\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"body {\\n  background-color: red;\\n}\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://mini-editor/./src/index.css?./node_modules/css-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);\n// Imports\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, \"* {\\n  margin: 0;\\n  padding: 0;\\n  box-sizing: border-box;\\n}\\n\\n.container {\\n  margin: 20px auto;\\n  width: 750px;\\n}\\n\\n.tool-box {\\n  display: flex;\\n  flex-direction: row;\\n}\\n\\n.tool-box-title {\\n  width: 60px;\\n  height: 100%;\\n  list-style: none;\\n  font-size: 16px;\\n  text-align: center;\\n  line-height: 60px;\\n  color: #333;\\n}\\n\\n\", \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://mini-editor/./src/editor/index.css?./node_modules/css-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -30,13 +30,13 @@ eval("\n\n/*\n  MIT License http://www.opensource.org/licenses/mit-license.php\n
 
 /***/ }),
 
-/***/ "./src/index.css":
-/*!***********************!*\
-  !*** ./src/index.css ***!
-  \***********************/
+/***/ "./src/editor/index.css":
+/*!******************************!*\
+  !*** ./src/editor/index.css ***!
+  \******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./index.css */ \"./node_modules/css-loader/dist/cjs.js!./src/index.css\");\n\n            \n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default, options);\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});\n\n//# sourceURL=webpack://mini-editor/./src/index.css?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../../node_modules/css-loader/dist/cjs.js!./index.css */ \"./node_modules/css-loader/dist/cjs.js!./src/editor/index.css\");\n\n            \n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default, options);\n\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_index_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});\n\n//# sourceURL=webpack://mini-editor/./src/editor/index.css?");
 
 /***/ }),
 
@@ -50,13 +50,33 @@ eval("\n\nvar isOldIE = function isOldIE() {\n  var memo;\n  return function mem
 
 /***/ }),
 
+/***/ "./src/editor/index.ts":
+/*!*****************************!*\
+  !*** ./src/editor/index.ts ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _utils_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils/index */ \"./src/utils/index.ts\");\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.css */ \"./src/editor/index.css\");\n\n\nvar Editor = /** @class */ (function () {\n    function Editor(selector) {\n        var _this = this;\n        // 菜单选项\n        this.toolConfig = [\n            { title: '标题', text: 'H' },\n            { title: '加粗', text: 'B' },\n            { title: '颜色', text: 'C' }\n        ];\n        // 文字大小1到7\n        this.setSize = function () {\n            var size = prompt(\"请输入文字大小\");\n            _this.exec(\"fontSize\", true, size);\n        };\n        this.setBold = function () {\n            _this.exec(\"bold\");\n        };\n        this.setColor = function () {\n            var color = prompt(\"请输入颜色\");\n            _this.exec(\"backColor\", true, color);\n        };\n        this.handleClick = function (text) {\n            _this.recoverRange();\n            if (text === 'B') {\n                _this.setBold();\n            }\n            else if (text === 'C') {\n                _this.setColor();\n            }\n            else if (text === 'H') {\n                _this.setSize();\n            }\n        };\n        this.create = function () {\n            _this.createContainer();\n            _this.createToolBar();\n            _this.createEditorArea();\n        };\n        this.createContainer = function () {\n            (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.attr)(_this.container, \"style\", \"border: 1px solid #c9d8db; height: 300px\");\n            (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.attr)(_this.container, \"class\", \"container\");\n        };\n        // 创建工具栏\n        this.createToolBar = function () {\n            var toolsBar = (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.createElement)('div');\n            _this.toolsBar = toolsBar;\n            (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.attr)(_this.toolsBar, \"style\", \"border-bottom: 1px solid #eee; height: 60px\");\n            _this.container.appendChild(_this.toolsBar);\n            var toolItemWrapper = (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.createElement)('ul');\n            (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.attr)(toolItemWrapper, \"class\", \"tool-box\");\n            var lis = '';\n            _this.toolConfig.forEach(function (_a) {\n                var title = _a.title, text = _a.text;\n                lis += \"<li class=\\\"tool-box-title\\\" title=\\\"\" + title + \"\\\">\" + text + \"</li>\";\n            });\n            // 事件代理绑定事件\n            toolItemWrapper.addEventListener('click', function (e) {\n                var item = e.target;\n                if (item.nodeName === 'LI') {\n                    _this.handleClick(item.innerText);\n                }\n            });\n            toolItemWrapper.innerHTML = lis;\n            _this.toolsBar.appendChild(toolItemWrapper);\n        };\n        // 创建编辑器区域\n        this.createEditorArea = function () {\n            var _a;\n            var editorArea = (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.createElement)('div');\n            _this.editorArea = editorArea;\n            (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.attr)(_this.editorArea, \"contenteditable\", \"true\");\n            (0,_utils_index__WEBPACK_IMPORTED_MODULE_0__.attr)(_this.editorArea, \"style\", \"height: 240px\");\n            _this.container.appendChild(_this.editorArea);\n            // 失去焦点后需要记录选中内容\n            (_a = _this.editorArea) === null || _a === void 0 ? void 0 : _a.addEventListener(\"blur\", function () {\n                _this.storeRange();\n            });\n        };\n        this.container = document.getElementById(selector);\n        this.selection = window.getSelection();\n        if (!this.container) {\n            throw Error('can not find dom selector, please check your selector ');\n        }\n    }\n    Editor.prototype.exec = function (cmd, showUI, value) {\n        if (showUI === void 0) { showUI = false; }\n        if (value === void 0) { value = \"\"; }\n        document.execCommand(cmd, showUI, value);\n    };\n    Editor.prototype.recoverRange = function () {\n        var _a = this, selection = _a.selection, range = _a.range;\n        if (selection && range) {\n            // 选区数量>0\n            if (selection.rangeCount > 0)\n                selection.removeAllRanges();\n            selection === null || selection === void 0 ? void 0 : selection.addRange(range.cloneRange());\n        }\n    };\n    Editor.prototype.storeRange = function () {\n        var selection = this.selection;\n        // 存在selection但是没有框选\n        if (selection && !selection.isCollapsed) {\n            var range = selection === null || selection === void 0 ? void 0 : selection.getRangeAt(0);\n            this.range = range;\n        }\n    };\n    return Editor;\n}());\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Editor);\n\n\n//# sourceURL=webpack://mini-editor/./src/editor/index.ts?");
+
+/***/ }),
+
 /***/ "./src/index.ts":
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.css */ \"./src/index.css\");\n\nvar add = function (a, b) {\n    return a + b;\n};\nconsole.log(add(1, 2));\n\n\n//# sourceURL=webpack://mini-editor/./src/index.ts?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _editor_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./editor/index */ \"./src/editor/index.ts\");\n\nvar div = document.createElement('div');\ndiv.setAttribute('id', 'root');\ndocument.body.append(div);\nvar editor = new _editor_index__WEBPACK_IMPORTED_MODULE_0__.default('root');\neditor.create();\n\n\n//# sourceURL=webpack://mini-editor/./src/index.ts?");
+
+/***/ }),
+
+/***/ "./src/utils/index.ts":
+/*!****************************!*\
+  !*** ./src/utils/index.ts ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createElement\": () => (/* binding */ createElement),\n/* harmony export */   \"attr\": () => (/* binding */ attr)\n/* harmony export */ });\nvar createElement = function (type, text) {\n    var element = document.createElement(type);\n    element.textContent = text;\n    return element;\n};\nvar attr = function (dom, key, value) {\n    dom.setAttribute(key, value);\n};\n\n\n//# sourceURL=webpack://mini-editor/./src/utils/index.ts?");
 
 /***/ })
 
